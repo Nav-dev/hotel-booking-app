5ef15615b77430c660ed42592e97276d9c06b21f
Nav-dev <sharmanav325@gmail.com>
2024-03-11 21:13:41 +0530

    added my-bookings page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,14 +14,17 @@ import EditHotel from "./pages/EditHotel";
 import Search from "./pages/Search";
 import Detail from "./pages/Detail";
 import Booking from "./pages/Booking";
+import MyBookings from "./pages/MyBookings";
+import Home from "./pages/Home";
 
 const App = () =>{
   const {isLoggedIn} = useAppContext();
   return(
     <Router>
       <Routes>
-        <Route path="/" element={<Layout>
-          <p>Home Page</p>
+        <Route path="/" element={
+        <Layout>
+          <Home />
         </Layout>} 
         />
         <Route 
@@ -94,6 +97,14 @@ const App = () =>{
              </Layout>
         }
         />
+          <Route
+              path="/my-bookings"
+              element={
+                <Layout>
+                  <MyBookings />
+                </Layout>
+              }
+            />
         </>)}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
